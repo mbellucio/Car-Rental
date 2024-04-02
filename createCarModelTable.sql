14942d1b5533ce8255f64b4759d0e1952ac5f36b
mbellucio <=>
2024-04-02 00:28:24 -0300

    3 - Create table Car_Model and insert data provided

---
+++ createCarModelTable.sql
@@ -0,0 +1,21 @@
+CREATE TABLE
+  CAR_MODEL (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    MODEL_NAME VARCHAR(120) NOT NULL
+  );
+
+INSERT INTO
+  CAR_MODEL (MODEL_NAME)
+VALUES
+  ('Conversível'),
+  ('Sedã'),
+  ('Hatch'),
+  ('Coupé'),
+  ('Perua'),
+  ('SUV'),
+  ('Picape'),
+  ('Minivan'),
+  ('Utilitário'),
+  ('Buggy');
+
+  
